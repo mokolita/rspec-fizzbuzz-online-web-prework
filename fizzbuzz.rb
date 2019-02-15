473fb540e30023d706589693abbf2f856f47484a
Meg Galvez <meg@oakmaine.com>
2019-02-15 23:39:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,11 +6,11 @@ def fizzbuzz(int)
     "Fizz"
   end
   
-  if int % 5 == 0
+  else int % 5 == 0
     "Buzz"
   end
   
-  if int % 3 == 0 
+   int % 3 == 0 
     int % 5 == 0 
     "FizzBuzz"  
   end
