4e249cf0671aa7d07848d1bf05bdfe87261e9e15
Meg Galvez <meg@oakmaine.com>
2019-02-15 23:32:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+  end
+  if int % 5 == 0
+    "Buzz"
+  end
+  if int % 3 == 0 
+    int % 5 == 0 
+    "Fizzbuzz"  
+  end
+  
   
 end
